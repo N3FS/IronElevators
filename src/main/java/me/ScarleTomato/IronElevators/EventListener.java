c3bf2d9fa8d16b34da72c65bab519b5887569c3a
Nekoneko <nekonekogame@gmail.com>
2018-08-20 01:24:53 +0900

    Update to Minecraft 1.13

---
+++ src/main/java/me/ScarleTomato/IronElevators/EventListener.java
@@ -0,0 +1,66 @@
+package me.ScarleTomato.IronElevators;
+
+import lombok.RequiredArgsConstructor;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
+
+@RequiredArgsConstructor
+public final class EventListener implements Listener {
+
+    private final IronElevators plugin;
+
+    private boolean searchFloor(Block floor) {
+        return floor.getType() == plugin.ELEVATOR_MATERIAL
+                && floor.getRelative(BlockFace.UP).getType().isTransparent()
+                && floor.getRelative(BlockFace.UP, 2).getType().isTransparent();
+    }
+
+    private void teleport(Player player, Location to) {
+        player.teleport(to);
+        player.getWorld().playSound(to, plugin.ELEVATOR_WHOOSH, 1, 0);
+    }
+
+    @EventHandler
+    public void downElevator(PlayerToggleSneakEvent event) {
+        Player player = event.getPlayer();
+        Block block = player.getLocation().getBlock().getRelative(BlockFace.DOWN);
+        if (player.hasPermission("ironelevators.use") && !player.isSneaking() && block.getType() == plugin.ELEVATOR_MATERIAL) {
+            block = block.getRelative(BlockFace.DOWN, plugin.MIN_ELEVATION);
+            int search = plugin.MAX_ELEVATION;
+            while (search > 0 && !(searchFloor(block))) {
+                search--;
+                block = block.getRelative(BlockFace.DOWN);
+            }
+            if (search > 0) {
+                Location loc = player.getLocation();
+                loc.setY(loc.getY() - plugin.MAX_ELEVATION - 3 + search);
+                teleport(player, loc);
+            }
+        }
+    }
+
+    @EventHandler
+    public void upElevator(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Block block = event.getTo().getBlock().getRelative(BlockFace.DOWN);
+        if (player.hasPermission("ironelevators.use") && event.getFrom().getY() < event.getTo().getY() && block.getType() == plugin.ELEVATOR_MATERIAL) {
+            block = block.getRelative(BlockFace.UP, plugin.MIN_ELEVATION);
+            int search = plugin.MAX_ELEVATION;
+            while (search > 0 && !(searchFloor(block))) {
+                search--;
+                block = block.getRelative(BlockFace.UP);
+            }
+            if (search > 0) {
+                Location loc = player.getLocation();
+                loc.setY(loc.getY() + plugin.MAX_ELEVATION + 3 - search);
+                teleport(player, loc);
+            }
+        }
+    }
+}
---
+++ src/main/java/me/ScarleTomato/IronElevators/IronElevators.java
@@ -0,0 +1,74 @@
+package me.ScarleTomato.IronElevators;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public final class IronElevators extends JavaPlugin {
+
+    int MIN_ELEVATION = 3, MAX_ELEVATION = 14;
+    Material ELEVATOR_MATERIAL = Material.IRON_BLOCK;
+    Sound ELEVATOR_WHOOSH = defaultSound();
+
+    @Override
+    public void onEnable() {
+        loadConfig();
+        getServer().getPluginManager().registerEvents(new EventListener(this), this);
+    }
+
+    @Override
+    public void saveDefaultConfig() {
+        Path path = getDataFolder().toPath().resolve("config.yml");
+        if (Files.notExists(path)) {
+            try {
+                Files.createDirectory(path.getParent());
+            } catch (IOException ex) {
+                getLogger().warning("Can't create base directory: " + ex.getLocalizedMessage());
+            }
+            try {
+                Files.createFile(path);
+                YamlConfiguration config = YamlConfiguration.loadConfiguration(path.toFile());
+                config.set("minElevation", MIN_ELEVATION);
+                config.set("maxElevation", MAX_ELEVATION);
+                config.set("elevatorMaterial", ELEVATOR_MATERIAL.name());
+                config.set("elevatorWhoosh", ELEVATOR_WHOOSH.name());
+                config.save(path.toFile());
+            } catch (IOException ex) {
+                getLogger().warning("Can't create config file: " + ex.getLocalizedMessage());
+            }
+        }
+    }
+
+    private void loadConfig() {
+        this.saveDefaultConfig();
+        this.reloadConfig();
+
+        try {
+            this.MIN_ELEVATION = this.getConfig().getInt("minElevation");
+            this.MAX_ELEVATION = this.getConfig().getInt("maxElevation");
+            this.ELEVATOR_MATERIAL = Material.valueOf(this.getConfig().getString("elevatorMaterial"));
+            this.ELEVATOR_WHOOSH = Sound.valueOf(this.getConfig().getString("elevatorWhoosh"));
+        } catch (Exception ex) {
+            getLogger().warning("Failed to load config: " + ex.getLocalizedMessage());
+        }
+    }
+
+    private static Sound defaultSound() {
+        String sound;
+        String ver = Bukkit.getServer().getVersion();
+        if (ver.contains("MC: 1.13") || ver.contains("MC: 1.13.")) {
+            sound = "ENTITY_IRON_GOLEM_ATTACK";
+        } else if (ver.contains("MC: 1.12") || ver.contains("MC: 1.12.")) {
+            sound = "ENTITY_IRONGOLEM_ATTACK";
+        } else {
+            throw new IllegalArgumentException("Unsupported version");
+        }
+        return Sound.valueOf(sound);
+    }
+}
